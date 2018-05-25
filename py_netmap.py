50a4ca0addc8ef121afff6bfddea00f89213a1fa
xl350r <danhsecure@gmail.com>
2018-05-24 17:08:47 -0700

    Minor fixes

--- py_netmap.py
+++ py_netmap.py
@@ -19,29 +19,25 @@ def trace_route(host):
 	for i in range(1, 128):
 	    pkt = IP(dst=host, ttl=i) / UDP(dport=33434)
 	    # Send the packet and get a reply
-	    reply = sr1(pkt, verbose=0)
+	    reply = sr1(pkt, verbose=0, timeout=2)
 	    if reply is None:
-	        # No reply =(
-	        next(iterator, default)
+	        # Reply == none
+	        next
 	    elif reply.type == 3:
-	        # We've reached our destination
 	        return(reply.src)
 	    else:
-	        # We're in the middle somewhere
+	        # middle hop
 	        return(i , reply.src)
 
 
 def icmp_discovery(hosts):
-	conf.verb=0
-	target_discovered=False
-	num_discovered=0
-	ipup=[]
-	ipdown=[]
+	target_discovered=False # Switch for initiak Trace when host a target is found.
+	ipup=[] # record who is up
+	ipdown=[] # record which IPs are down
 	for i in hosts:
 		packet = IP(dst=i, ttl=128)/ICMP()
 		reply = sr1(packet, timeout=2)
 	if not (reply is None):
-		num_discovered += 1
 		if target_discovered == False:
 			target_discovered = True
 			print(reply.dst, " is up")
